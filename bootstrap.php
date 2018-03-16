daa3f9bbef8522a84a9a7b576be8a5f81eb45127
Marcel Hernandez <e6990620@gmail.com>
2018-03-16 15:34:10 +0100

    ditch PHP container and just bind the RabbitMQ endpoint to localhost instead

--- bootstrap.php
+++ bootstrap.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 
 
-$conn = new AMQPStreamConnection('rabbitmq', 5672, 'guest', 'guest');
+$conn = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
 $chan = $conn->channel();
 
 $chan->queue_declare('hello', false, true, false, false);
